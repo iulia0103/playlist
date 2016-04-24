7449f6920171f3f58f884ff2e1be2aed498ee16b
iulia0103 <iulia.leancu@gmail.com>
2016-04-24 16:27:06 +0200

    add a new migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160420060456) do
+ActiveRecord::Schema.define(version: 20160424142125) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20160420060456) do
     t.integer  "artist_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "video"
   end
 
   add_index "songs", ["artist_id"], name: "index_songs_on_artist_id", using: :btree
